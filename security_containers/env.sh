717b2e0e9cfe78aa8587317140c989063d39d1fe
mnuirian <mnuirian@redhat.com>
2017-06-19 10:27:39 -0400

    Update env.sh

--- security_containers/env.sh
+++ security_containers/env.sh
@@ -3,6 +3,7 @@
 # USAGE: source env.sh
 
 # These are for my rhdemo aws account
+# Sign-in at: https://cloud-nik.signin.aws.amazon.com/console
 export AWS_ACCESS_KEY_ID="AKIAIWAKRFCI3EUBRDXA";
 export AWS_SECRET_ACCESS_KEY="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";
 
